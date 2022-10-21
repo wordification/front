9eec4c655d9901992bc3c48084209d129521614a
Dalton Craven <daltoncraven@proton.me>
2022-10-21 13:09:18 -0400

    feat: update header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,7 @@ const Home: NextPage = () => (
     </Head>
 
     <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <h1 className={styles.title}>Wordification!</h1>
 
       <p className={styles.description}>
         Get started by editing{" "}
