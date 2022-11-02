42fbb9d70954dc87dfa370a564873abe83e23444
Dalton Craven <daltoncraven@proton.me>
2022-11-01 21:55:26 -0400

    fix: temporarily disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line no-restricted-exports
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+export {}; // TODO: re-enable middleware
