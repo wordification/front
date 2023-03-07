b4518381b2122efbd5e616284d75640edaff63c8
Dalton Craven <daltoncraven@proton.me>
2023-03-07 18:57:54 -0500

    chore: remove axios

--- lib/axios.ts
+++
@@ -1,9 +0,0 @@
-import axiosBase from "axios";
-
-const axios = axiosBase.create({
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  baseURL: process.env.API_BASE_URL!,
-  timeout: 1000,
-});
-
-export default axios;
