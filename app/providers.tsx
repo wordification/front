9694ec747533253d6a74f2e043832bc4de12c762
Dalton Craven <daltoncraven@proton.me>
2023-04-06 16:25:35 -0400

    chore: install react-hot-toast

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,26 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import dynamic from "next/dynamic";
 
 const queryClient = new QueryClient();
 
+const Toaster = dynamic(
+  async () => {
+    const { Toaster: BaseToaster } = await import("react-hot-toast");
+    return BaseToaster;
+  },
+  {
+    ssr: false,
+  }
+);
+
 const Providers = ({ children }: { children: React.ReactNode }) => (
   <SessionProvider>
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
   </SessionProvider>
 );
 
