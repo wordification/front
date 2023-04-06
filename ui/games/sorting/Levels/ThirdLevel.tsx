68c01645d323ae6793d1c88e0cacb1864638edb3
Dalton Craven <daltoncraven@proton.me>
2023-04-06 16:59:58 -0400

    feat: create third level template

---
+++ ui/games/sorting/Levels/ThirdLevel.tsx
@@ -0,0 +1,27 @@
+import type { WordElement } from "@/lib/games/sorting/types";
+
+import fetchServer from "@/lib/fetch/fetchServer";
+import GameCard from "@/ui/games/sorting/Cards/GameCard";
+
+const getData = async (gameId: string) => {
+  const testedWord = await fetchServer<WordElement>(
+    `/sorting_game/${gameId}/tested_word/`
+  ).then((res) => res.json());
+
+  return { testedWord };
+};
+
+const ThirdLevel = async ({ gameId }: { gameId: string }) => {
+  const data = await getData(gameId);
+
+  return (
+    <>
+      <GameCard title="Spell the word.">
+        <p>Coming soon...</p>
+      </GameCard>
+      <p className="text-sm">Current word: {data.testedWord.word.str_word}</p>
+    </>
+  );
+};
+
+export default ThirdLevel;
