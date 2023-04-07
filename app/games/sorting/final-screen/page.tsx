82c6937f5b19027ef7c6ca06b48588955a3c9768
ShashComandur <shash@Shash-Laptop.localdomain>
2023-04-07 14:16:04 -0400

    feat:added final screen

---
+++ app/games/sorting/final-screen/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+const Page = () => {
+  const [currentCard, setCurrentCard] = useState(0);
+
+  return (
+    <>
+      <div className="card w-full bg-base-300 shadow-xl">
+        <div className="card-body">
+          <div className="grid grid-cols-2 mt-6">
+            <form className="justify-end" action="/send-data-here" method="post">
+              <input className="rounded p-4" type="text" id="first" name="first" placeholder="Type your answer here."/> 
+            </form>
+            <div className="card-actions justify-start"> 
+                <button className="btn btn-primary" type="button">
+                  Check Spelling
+                </button>
+              </div>
+          </div>
+          <div className="card-actions justify-center mt-2">
+            <button className="btn btn-secondary" type="button">
+              Play Sound Again
+            </button>
+          </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Page;
