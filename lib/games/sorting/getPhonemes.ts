3c291ebd74b8b08dd192e449f7acb34860cca0c1
Dalton Craven <daltoncraven@proton.me>
2023-04-17 15:12:29 -0400

    fix: split setup page into two components

--- lib/games/sorting/getPhonemes.ts
+++ lib/games/sorting/getPhonemes.ts
@@ -4,6 +4,7 @@ const getPhonemes = () =>
   fetchServer<{ id: number; name: string }[]>(
     "/sorting_game/phoneme_options/"
   ).then((res) => res.json());
+
 export const getSpecificPhonemes = (phonemeIds: readonly number[]) =>
   getPhonemes().then((phonemes) =>
     phonemes.filter((phoneme) => phonemeIds.includes(phoneme.id))
